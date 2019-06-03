741897acf0933ecb7209ff492cbae66d8945d63a
Kyle-Pu <kylez.pu@gmail.com>
2019-06-02 21:57:09 -0700

    Final edits to data insights program of NFL suspension history using Pandas

--- Pandas_Data_Insights/NFL_Suspensions_Data_Insights/groupingAndSorting.py
+++ Pandas_Data_Insights/NFL_Suspensions_Data_Insights/groupingAndSorting.py
@@ -2,8 +2,6 @@ print("Loading modules and data...")
 import pandas as pd
 suspensions = pd.read_csv("nfl-suspensions-data.csv")
 
-print(suspensions.head())
-
 # Some summary information
 print("\n\nHere's a look at some entries from the original dataset:\n")
 print(suspensions.head())
@@ -11,14 +9,29 @@ print(suspensions.head())
 print("\n\nA quick overview of our data:\n")
 print(suspensions.describe())
 
+
+# Grouping Functions
 print("\n\nHow many suspensions each team has:")
 team_suspensions = suspensions.groupby("team").size()     # groupby is a really neat function that groups our data by common attributes!
 print(team_suspensions)
+
+print("\n\nTeams sorted by most suspensions to least suspensions:")
+teamSuspense = suspensions.groupby("team").team.count().sort_values(ascending = False)
+print(teamSuspense)
 
 print("\n\nYears sorted by most suspensions to least suspensions:")
 offenses = suspensions.groupby("year").size().sort_values(ascending = False)
 print(offenses)
 
+
+# CURRENTLY BUGGY (CHECK GITHUB ISSUE PLEASE)!!!
 print("\n\nLongest and shortest suspensions of a member on each team:")
 longSuspense = suspensions.groupby(['team']).games.agg([min, max])
 print(longSuspense)
+
+
+# Multi-Indexing
+print("\n\nMulti-Indexed grouping by team AND player to see detailed information about each team, its players, and its player's number of suspensions:")
+multiSuspense = suspensions.groupby(['team', 'name']).name.count()
+print(multiSuspense)
+
